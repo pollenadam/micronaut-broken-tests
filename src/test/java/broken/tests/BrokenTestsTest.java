41b6b4b84817ed5d57e33dd01e5f0bda84b64d62
Adam Parkin <adam@pollen.vc>
2022-06-08 13:31:43 +0100

    initial commit

---
+++ src/test/java/broken/tests/BrokenTestsTest.java
@@ -0,0 +1,26 @@
+package broken.tests;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest(rebuildContext = true)
+class BrokenTestsTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+    @Test
+    void testButDoesItReally() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
